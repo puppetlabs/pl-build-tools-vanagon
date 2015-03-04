a21a986f87abdc773fa555e414b2501e1b058ad6
Michael Stahnke <stahnma@puppetlabs.com>
2015-03-04 23:55:14 +0000

    (RE-3931) Build out the cfacter toolchain for Arista
    This commit adds the eos platform definition to build tools.

---
+++ configs/platforms/eos-4-i386.rb
@@ -0,0 +1,12 @@
+platform "eos-4-i386" do |plat|
+  plat.servicedir "/etc/rc.d/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
+
+  plat.provision_with "yum install -y --nogpgcheck autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils"
+  plat.yum_repo "http://osmirror.delivery.puppetlabs.net/fedora14-i386/eos-4-i386.repo"
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/eos/4/i386/pl-build-tools-el-4.repo"
+
+  plat.install_build_dependencies_with "yum install -y --nogpgcheck"
+  plat.vcloud_name "fedora-14-i386"
+end
