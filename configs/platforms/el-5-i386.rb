91a378fc46be39424365120b3e0df1a2b5e10f5d
Michael Stahnke <stahnma@puppetlabs.com>
2015-04-13 16:14:38 +0000

    (RE-4381) Migrate EL builds into pl-build-tools-vanagon
    This commit moves EL platform definitions into the vanagon repos with
    hope of dropping pl-dependencies.

---
+++ configs/platforms/el-5-i386.rb
@@ -0,0 +1,10 @@
+platform "el-5-i386" do |plat|
+  plat.servicedir "/etc/rc.d/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
+
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/el/5/i386/pl-build-tools-release-5-1.noarch.rpm"
+  plat.provision_with "yum install -y --nogpgcheck autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign rpm-build"
+  plat.install_build_dependencies_with "yum install -y --nogpgcheck "
+  plat.vcloud_name "centos-5-i386"
+end
