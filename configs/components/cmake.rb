05c120c9f3b58a99ac73005b383ac0f3d90a1930
Heath Seals <heath@puppetlabs.com>
2015-05-08 18:25:38 +0200

    (RE-4526) Add cmake for Mac via pl-build-tools-vanagon (mavericks)

--- configs/components/cmake.rb
+++ configs/components/cmake.rb
@@ -3,29 +3,57 @@ component "cmake" do |pkg, settings, platform|
   pkg.md5sum "2da57308071ea98b10253a87d2419281"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{pkg.get_name}-#{pkg.get_version}.tar.gz"
 
-  if platform.is_deb?
-    pkg.build_requires "libncurses5-dev"
-  else
-    pkg.build_requires "ncurses-devel"
+  unless platform.is_osx?
+    pkg.build_requires "pl-gcc"
+    pkg.build_requires "make"
+    if platform.is_deb?
+      pkg.build_requires "libncurses5-dev"
+    else
+      pkg.build_requires "ncurses-devel"
+    end
   end
-  pkg.build_requires "pl-gcc"
-  pkg.build_requires "make"
 
-  if platform.is_aix?
+  # This is pretty horrible.  But so is package management on OSX.
+  pkg.build_requires "pl-gcc-4.8.2" if platform.is_osx?
+
+  if platform.is_aix? or platform.is_osx?
     ldflags='LDFLAGS="${LDFLAGS}"'
   else
     ldflags="LDFLAGS=-Wl,-rpath=#{settings[:bindir]}/lib,-rpath=#{settings[:bindir]}/lib64,--enable-new-dtags"
   end
 
+  # Different toolchains for different target platforms.
+  if platform.is_osx?
+    toolchain="pl-build-toolchain-darwin"
+  else
+    toolchain="pl-build-toolchain"
+  end
+
+  # Initialize an empty env_setup string
+  env_setup = ""
+
+  env_setup << %Q{export PATH=$$PATH:/usr/local/bin; \
+  export CC=#{settings[:bindir]}/gcc; export CXX=#{settings[:bindir]}/g++; \
+  export #{ldflags}}
+
+  # Initialize an empty configure_command string
+  configure_command  = ""
+
+  configure_command << " ./configure --prefix=#{settings[:prefix]} --docdir=share/doc"
+
+  # Even though only system curl is available on the build host,
+  # the build on OSX bombs without this.
+  if platform.is_osx?
+    configure_command << " --system-curl"
+  end
 
   pkg.build do
-    [ "export CC=#{settings[:bindir]}/gcc",
-      "export CXX=#{settings[:bindir]}/g++",
-      "export #{ldflags}",
-      "./configure --prefix=#{settings[:prefix]} --docdir=share/doc",
+    [ 
+      env_setup,
+      configure_command,
       "#{platform[:make]} VERBOSE=1 -j$(shell expr $(shell #{platform[:num_cores]}) + 1)",
-      "cd #{settings[:prefix]}; curl -O  http://buildsources.delivery.puppetlabs.net/pl-build-toolchain.cmake",
-      "chmod 644 #{settings[:prefix]}/pl-build-toolchain.cmake"
+      "cd #{settings[:prefix]}; curl -O  http://buildsources.delivery.puppetlabs.net/#{toolchain}.cmake",
+      "chmod 644 #{settings[:prefix]}/#{toolchain}.cmake"
     ]
   end
 
--- configs/platforms/osx-10.10-x86_64.rb
+++ configs/platforms/osx-10.10-x86_64.rb
@@ -2,6 +2,7 @@ platform "osx-10.10-x86_64" do |plat|
   plat.servicetype 'launchd'
   plat.servicedir '/Library/LaunchDaemons'
   plat.provision_with 'mkdir /usr/local; cd /usr/local; git clone https://github.com/Homebrew/homebrew.git .'
-  plat.install_build_dependencies_with "#"
+  plat.provision_with 'curl -o /usr/local/bin/osx-deps http://pl-build-tools.delivery.puppetlabs.net/osx/osx-deps; chmod 755 /usr/local/bin/osx-deps'
+  plat.install_build_dependencies_with "/usr/local/bin/osx-deps "
   plat.vcloud_name "osx-1010-x86_64"
 end
