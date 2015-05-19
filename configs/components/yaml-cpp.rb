96a5433bb9ddc124fbf1e9911155488a7c2ec521
Heath Seals <heath@puppetlabs.com>
2015-05-19 15:59:49 +0200

    (RE-4527) Add yaml-cpp for Mac via pl-build-tools-vanagon (mavericks)

--- configs/components/yaml-cpp.rb
+++ configs/components/yaml-cpp.rb
@@ -3,13 +3,23 @@ component "yaml-cpp" do |pkg, settings, platform|
   pkg.md5sum "0fa47a5ed8fedefab766592785c85ee7"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{pkg.get_name}-#{pkg.get_version}.tar.gz"
 
-  pkg.build_requires "pl-gcc"
-  pkg.build_requires "make"
-  pkg.build_requires "pl-cmake"
-  pkg.build_requires "pl-boost"
+  # This is pretty horrible.  But so is package management on OSX.
+  if platform.is_osx?
+    pkg.build_requires "pl-gcc-4.8.2"
+    pkg.build_requires "pl-cmake-3.2.2"
+    pkg.build_requires "pl-boost-1.57.0"
+  else
+    pkg.build_requires "pl-gcc"
+    pkg.build_requires "make"
+    pkg.build_requires "pl-cmake"
+    pkg.build_requires "pl-boost"
+  end
 
-  pkg.configure do
-    [ "sed -i 's/\r//' license.txt"  ]
+  # Different toolchains for different target platforms.
+  if platform.is_osx?
+    toolchain="pl-build-toolchain-darwin"
+  else
+    toolchain="pl-build-toolchain"
   end
 
   pkg.build do
@@ -17,7 +27,7 @@ component "yaml-cpp" do |pkg, settings, platform|
       "mkdir build-shared",
       "cd build-shared",
       "#{settings[:prefix]}/bin/cmake \
-    -DCMAKE_TOOLCHAIN_FILE=#{settings[:prefix]}/pl-build-toolchain.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=#{settings[:prefix]}/#{toolchain}.cmake \
     -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -DYAML_CPP_BUILD_TOOLS=0 \
@@ -28,7 +38,7 @@ component "yaml-cpp" do |pkg, settings, platform|
       "rm -rf build-static",
       "mkdir build-static",
       "cd build-static", "#{settings[:prefix]}/bin/cmake \
-    -DCMAKE_TOOLCHAIN_FILE=#{settings[:prefix]}/pl-build-toolchain.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=#{settings[:prefix]}/#{toolchain}.cmake \
     -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -DYAML_CPP_BUILD_TOOLS=0 \
