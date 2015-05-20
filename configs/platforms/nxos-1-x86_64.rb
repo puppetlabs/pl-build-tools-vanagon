8429c5c2ddc449c8c696acd1a303801dd2f168bf
Scott Garman <sgarman@zenlinux.com>
2015-05-20 13:12:09 -0700

    Configure install_build_dependencies_with for NXOS
    The NXOS build environment we're using does not have any yum
    repositories enabled, so using yum install -y here doesn't
    work, as yum install will always fail without any repositories
    to work with. Also, the NXOS build environment has already
    been customized with all the build dependencies, so we can
    assume the dependencies are met anyway.
    
    This turns the build dependency check into a no-op for now. A
    future build of the NXOS build environment may end up having
    yum repositories enabled, at which time we can make this do what
    it does for our other platforms.

--- configs/platforms/nxos-1-x86_64.rb
+++ configs/platforms/nxos-1-x86_64.rb
@@ -4,6 +4,8 @@ platform "nxos-1-x86_64" do |plat|
   plat.servicetype "sysv"
   # Remove these -dev packages that can interfere with our gcc bootstrapping
   plat.provision_with "rpm -e libgmp-dev libmpc-dev libmpfr-dev"
+  # NXOS doesn't have any yum repos enabled by default, so this is a no-op:
+  plat.install_build_dependencies_with "echo Please ensure the following packages are installed:"
   plat.docker_image "nxos_base_image"
   plat.ssh_port 2222
 end
