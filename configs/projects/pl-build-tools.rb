05b08bc1a897a625e225e18b767ed67512184b5c
Matthaus Owens <matthaus@puppetlabs.com>
2015-07-21 14:54:34 -0700

    (RE-4563) Update project settings to include basedir and platform_triple
    When building binutils for solaris, we will be passing --target, which
    will add a suffix to the supplied prefix. This means we will want a
    directory that is just /opt/pl-build-tools for that purpose. We also
    want bindir, libdir and friends to include the triple in their path for
    solaris, so we do want prefix to include it. This commit adds a new
    setting called basedir to allow that. It also includes a new setting
    called platform_triple which can be used in binutils and gcc builds as
    the target.

--- configs/projects/pl-build-tools.rb
+++ configs/projects/pl-build-tools.rb
@@ -1,5 +1,20 @@
 # Contains all common project settings in one place
-proj.setting(:prefix, "/opt/pl-build-tools")
+
+platform = proj.get_platform
+
+# For solaris, we build cross-compilers
+if platform.is_solaris?
+  if platform.architecture == 'i386'
+    platform_triple = "#{platform.architecture}-pc-solaris2.#{platform.os_version}"
+  else
+    platform_triple = "#{platform.architecture}-sun-solaris2.#{platform.os_version}"
+  end
+end
+
+# Project level settings our components will care about
+proj.setting(:platform_triple, platform_triple)
+proj.setting(:basedir, "/opt/pl-build-tools")
+proj.setting(:prefix, "/opt/pl-build-tools/#{platform_triple}")
 proj.setting(:sysconfdir, "/etc/pl-build-tools")
 proj.setting(:logdir, "/var/log/pl-build-tools")
 proj.setting(:piddir, "/var/run/pl-build-tools")
@@ -8,3 +23,5 @@ proj.setting(:libdir, File.join(proj.prefix, "lib"))
 proj.setting(:includedir, File.join(proj.prefix, "include"))
 proj.setting(:datadir, File.join(proj.prefix, "share"))
 proj.setting(:mandir, File.join(proj.datadir, "man"))
+
+proj.directory proj.basedir
