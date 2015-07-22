896d8786d3a0a9644adaca6e6854348029c2e1b9
Matthaus Owens <matthaus@puppetlabs.com>
2015-07-22 15:37:32 -0700

    (RE-4565) Add solaris 10 i386 support to boost component
    This commit updates the boost component with solaris support. Mostly
    this means bringing in solaris build dependencies and refactoring cflags
    and ldflags to be different for solaris than for linux.

--- configs/components/boost.rb
+++ configs/components/boost.rb
@@ -7,14 +7,36 @@ component "boost" do |pkg, settings, platform|
   # This is pretty horrible.  But so is package management on OSX.
   if platform.is_osx?
     pkg.build_requires "pl-gcc-4.8.2"
+  elsif platform.is_solaris?
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWarc.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWgnu-idn.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWgpch.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWgtar.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWhea.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWlibm.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWwgetu.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/depends/SUNWxcu4.pkg.gz'
+
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/pl-gcc-4.8.2.i386.pkg.gz'
+    pkg.build_requires 'http://pl-build-tools.delivery.puppetlabs.net/solaris/10/pl-binutils-2.25.i386.pkg.gz'
+
+    pkg.environment "PATH" => "#{settings[:bindir]}:/usr/ccs/bin:/usr/sfw/bin:$$PATH"
+    linkflags = "-Wl,-rpath=#{settings[:libdir]}"
+    b2flags = "define=_XOPEN_SOURCE=600"
+    cflags = ""
   else
     pkg.build_requires "pl-gcc"
+
+    pkg.environment "PATH" => "#{settings[:bindir]}:$$PATH"
+    linkflags = "-Wl,-rpath=#{settings[:libdir]},-rpath=#{settings[:libdir]}64"
+    b2flags = ""
+    cflags = "-fPIC"
   end
 
   pkg.build do
     [
-      %Q{echo 'using gcc : 4.8.2 : #{settings[:bindir]}/g++ : <linkflags>"-Wl,-rpath=#{settings[:libdir]},-rpath=#{settings[:libdir]}64" <cflags>"-fPIC" <cxxflags>"-fPIC" ;' > ~/user-config.jam},
-      "cd tools/build", "PATH=#{settings[:bindir]}:$$PATH",
+      %Q{echo 'using gcc : 4.8.2 : #{settings[:bindir]}/g++ : <linkflags>"#{linkflags}" <cflags>"#{cflags}" <cxxflags>"#{cflags}" ;' > ~/user-config.jam},
+      "cd tools/build",
       "./bootstrap.sh --with-toolset=gcc",
       "./b2 install -d+2 --prefix=#{settings[:prefix]} toolset=gcc --debug-configuration"
     ]
@@ -23,6 +45,7 @@ component "boost" do |pkg, settings, platform|
   pkg.install do
     [ "#{settings[:prefix]}/bin/b2 \
     -d+2 \
+    #{b2flags} \
     --debug-configuration \
     --build-dir=. \
     --prefix=#{settings[:prefix]} \
