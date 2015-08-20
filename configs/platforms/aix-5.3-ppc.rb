8cec6cdd97faa29c2179ed7079a0fe4312819696
Michael Stahnke <stahnma@puppetlabs.com>
2015-08-20 23:41:10 +0000

    (RE-5288) Add platform definitions for AIX 7
    This adds a platform definition for AIX 7 and normalizes the other AIX
    definitions to use rpm to install build_requires. It also implements a
    strange cleanup function by overriding provision_with to perform some
    removal magic.

--- configs/platforms/aix-5.3-ppc.rb
+++ configs/platforms/aix-5.3-ppc.rb
@@ -7,8 +7,12 @@ platform "aix-5.3-ppc" do |plat|
   plat.make "gmake"
   plat.tar "/opt/freeware/bin/tar"
   plat.rpmbuild "/usr/bin/rpm"
+  plat.patch "/opt/freeware/bin/patch"
 
-  plat.install_build_dependencies_with "# Please ensure the following packages are installed:"
+  plat.provision_with "rm -rf /opt/pl-build-tools; rm -rf /var/tmp/tmp.* /var/tmp/*root-root /var/tmp/rpm*"
+
+  plat.install_build_dependencies_with "rpm -Uvh --replacepkgs "
+
   # To use vanagon, mktemp is needed, so just leave this line in.
   plat.aix_package 'http://int-resources.corp.puppetlabs.net/AIX_MIRROR', 'mktemp-1.7-1.aix5.1.ppc.rpm'
 
--- configs/platforms/aix-7.1-ppc.rb
+++ configs/platforms/aix-7.1-ppc.rb
@@ -1,4 +1,4 @@
-platform "aix-6.1-ppc" do |plat|
+platform "aix-7.1-ppc" do |plat|
   # These probably aren't right, but we haven't gotten to building any daemons yet.
   plat.servicedir "/etc/init.d"
   plat.defaultdir "/etc/sysconfig"
@@ -15,4 +15,5 @@ platform "aix-6.1-ppc" do |plat|
 
   # To use vanagon, mktemp is needed, so just leave this line in.
   plat.aix_package 'http://int-resources.corp.puppetlabs.net/AIX_MIRROR', 'mktemp-1.7-1.aix5.1.ppc.rpm'
+
 end
