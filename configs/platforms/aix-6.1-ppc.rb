97e99e7f76fd4e27d8e136d24e0b8c564ead25f0
Michael Stahnke <stahnma@puppetlabs.com>
2015-08-21 19:38:30 +0000

    (maint) Remove dirty aix_package useage
    This commit updates the AIX platform definitions to just use a normal
    provision_with statement and not use the aix_package method. This
    means the aix_package method can/should be removed from vanagon.

--- configs/platforms/aix-6.1-ppc.rb
+++ configs/platforms/aix-6.1-ppc.rb
@@ -9,10 +9,11 @@ platform "aix-6.1-ppc" do |plat|
   plat.rpmbuild "/usr/bin/rpm"
   plat.patch "/opt/freeware/bin/patch"
 
-  plat.provision_with "rm -rf /opt/pl-build-tools; rm -rf /var/tmp/tmp.* /var/tmp/*root-root /var/tmp/rpm*"
+  # Basic vanagon operations require mktemp, so leave this in there
+  plat.provision_with "rpm -Uvh --replacepkgs http://int-resources.corp.puppetlabs.net/AIX_MIRROR/mktemp-1.7-1.aix5.1.ppc.rpm"
+
+  # This is how we clean up our last build since we don't have a pooler image
+  plat.provision_with "rm -rf /opt/pl-build-tools; rm -rf /var/tmp/tmp.* /var/tmp/*root-root /var/tmp/rpm* /root/*.jam"
 
   plat.install_build_dependencies_with "rpm -Uvh --replacepkgs "
-
-  # To use vanagon, mktemp is needed, so just leave this line in.
-  plat.aix_package 'http://int-resources.corp.puppetlabs.net/AIX_MIRROR', 'mktemp-1.7-1.aix5.1.ppc.rpm'
 end
