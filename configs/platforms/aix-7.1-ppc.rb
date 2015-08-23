7bed1e0014f11eda88ec0e881184a4dbab7375c2
Michael Stahnke <stahnma@puppetlabs.com>
2015-08-23 01:45:24 +0000

    (maint) Clean up platform definitions
    Platform definitions now contain the basic utilities needed for a
    vanagon build this includes mktemp, rsync, coreutils and sed. Without
    those, any build will fail. These are GNU versions of mktemp, coreutils
    and sed.
    
    This allows cleaner components by pulling out BRs on those packages as
    well.

--- configs/platforms/aix-7.1-ppc.rb
+++ configs/platforms/aix-7.1-ppc.rb
@@ -9,8 +9,8 @@ platform "aix-7.1-ppc" do |plat|
   plat.rpmbuild "/usr/bin/rpm"
   plat.patch "/opt/freeware/bin/patch"
 
-  # Basic vanagon operations require mktemp, so leave this in there
-  plat.provision_with "rpm -Uvh --replacepkgs http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/mktemp-1.7-1.aix5.1.ppc.rpm"
+  # Basic vanagon operations require mktemp, rsync, coreutils, tar and sed so leave this in there
+  plat.provision_with "rpm -Uvh --replacepkgs http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/mktemp-1.7-1.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/rsync-3.0.6-1.aix5.3.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/coreutils-5.2.1-2.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/sed-4.1.1-1.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/make-3.80-1.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/tar-1.22-1.aix6.1.ppc.rpm"
 
   # This is how we clean up our last build since we don't have a pooler image
   plat.provision_with "rm -rf /opt/pl-build-tools; rm -rf /var/tmp/tmp.* /var/tmp/*root-root /var/tmp/rpm* /root/*.jam"
