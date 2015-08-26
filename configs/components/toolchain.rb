5dda8fc0cb1d459e552f1b1641a66705338d36ca
Matthaus Owens <matthaus@puppetlabs.com>
2015-08-26 15:48:16 -0700

    (maint) Add solaris 11 toolchain files
    Here we add the toolchain files for solaris 11 to the toolchain
    component and install them similarly to how the solaris 10 toolchain
    files are installed.

--- configs/components/toolchain.rb
+++ configs/components/toolchain.rb
@@ -13,6 +13,9 @@ component "toolchain" do |pkg, settings, platform|
       pkg.url "file://files/solaris-10-toolchains.tar.gz"
       pkg.md5sum "dfb2880781e45baa2c702c5a8bd8f5d6"
       pkg.version '2015-08-12'
+    elsif platform.os_version == "11"
+      pkg.add_source "file://files/solaris-11-i386-toolchain.cmake.txt", sum: "189b7abcc50e4915d521db2fd4f30a9e"
+      pkg.add_source "file://files/solaris-11-sparc-toolchain.cmake.txt", sum: "3c3cd0e958471844346b7193e2feab0d"
     else
       fail "Need to define a toolchain file for #{platform.name} first"
     end
@@ -20,16 +23,16 @@ component "toolchain" do |pkg, settings, platform|
     fail "Need to define a toolchain file for #{platform.name} first"
   end
   filename = pkg.get_url.split('/').last
-  # We still need to add support for OS X and Solaris 11/{i386,sparc}.
+  # We still need to add support for OS X
   if platform.is_solaris?
-    pkg.install_file "solaris-10-i386-toolchain.cmake.txt", "#{settings[:basedir]}/i386-pc-solaris2.10/pl-build-toolchain.cmake"
-    pkg.install_file "solaris-10-sparc-toolchain.cmake.txt", "#{settings[:basedir]}/sparc-sun-solaris2.10/pl-build-toolchain.cmake"
+    pkg.install_file "solaris-#{platform.os_version}-i386-toolchain.cmake.txt", "#{settings[:basedir]}/i386-pc-solaris2.#{platform.os_version}/pl-build-toolchain.cmake"
+    pkg.install_file "solaris-#{platform.os_version}-sparc-toolchain.cmake.txt", "#{settings[:basedir]}/sparc-sun-solaris2.#{platform.os_version}/pl-build-toolchain.cmake"
 
     pkg.install do
       [
         # We update ownership here to make sure that solaris will put the toolchains in the package
-        "chown root:root #{settings[:basedir]}/i386-pc-solaris2.10/pl-build-toolchain.cmake",
-        "chown root:root #{settings[:basedir]}/sparc-sun-solaris2.10/pl-build-toolchain.cmake",
+        "chown root:root #{settings[:basedir]}/i386-pc-solaris2.#{platform.os_version}/pl-build-toolchain.cmake",
+        "chown root:root #{settings[:basedir]}/sparc-sun-solaris2.#{platform.os_version}/pl-build-toolchain.cmake",
       ]
     end
   else
