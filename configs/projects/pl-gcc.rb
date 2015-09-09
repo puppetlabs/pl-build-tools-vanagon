1a6da0b1adc28472b4f7c6d95ab362980453490e
Matthaus Owens <matthaus@puppetlabs.com>
2015-09-09 16:51:37 -0700

    (maint) Reverse unless with else in gcc config
    if/else is preferred to unless/else, so this commit reverses the
    conditional in the gcc project.

--- configs/projects/pl-gcc.rb
+++ configs/projects/pl-gcc.rb
@@ -3,10 +3,11 @@ project "pl-gcc" do |proj|
   instance_eval File.read('configs/projects/pl-build-tools.rb')
 
   proj.description "Puppet Labs GCC"
-  unless platform.is_aix?
-    proj.version "4.8.2"
-  else
+
+  if platform.is_aix?
     proj.version "5.2.0"
+  else
+    proj.version "4.8.2"
   end
   proj.license "Same as GCC"
   proj.vendor "Puppet Labs <info@puppetlabs.com>"
