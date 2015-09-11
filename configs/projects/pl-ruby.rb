9bdee7b309d685c244ba6b8923da5548f9cef4e1
Matthaus Owens <matthaus@puppetlabs.com>
2015-09-11 11:33:02 -0700

    (RE-5401) Add pl-ruby project to pl-build-tools
    Cross compiling ruby requires ruby, and in the case of ruby 2, a ruby of
    1.9 or higher is required. Solaris 11 has 1.8 available for 11.1, which
    is where we build, so this commit adds a pl-ruby project so that we can
    depend on our own build of ruby.

---
+++ configs/projects/pl-ruby.rb
@@ -0,0 +1,20 @@
+project "pl-ruby" do |proj|
+  # Project level settings our components will care about
+  instance_eval File.read('configs/projects/pl-build-tools.rb')
+
+  proj.description "Puppet Labs Ruby used for cross-compiling"
+
+  proj.license "2 Clause BSD"
+  proj.vendor "Puppet Labs <info@puppetlabs.com>"
+  proj.homepage "https://www.puppetlabs.com"
+  proj.version "2.1.6"
+
+  # Platform specific - these flags do not work on AIX
+  unless platform.is_aix?
+    proj.setting(:cflags, "-I#{proj.includedir}")
+    proj.setting(:ldflags, "-L#{proj.libdir} -Wl,-rpath=#{proj.libdir}")
+  end
+
+  proj.component "ruby"
+  proj.target_repo ""
+end
