21fe28239e9d647082ed62866cad1301d4c9ad10
Rick Sherman <rick@shermdog.com>
2015-10-21 10:23:24 -0500

    (maint) use yum_repo for cisco-wrlinux

--- configs/platforms/cisco-wrlinux-5-x86_64.rb
+++ configs/platforms/cisco-wrlinux-5-x86_64.rb
@@ -3,7 +3,8 @@ platform "cisco-wrlinux-5-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
   # Remove these -dev packages that can interfere with our gcc bootstrapping
-  plat.provision_with "rpm -e libgmp-dev libmpc-dev libmpfr-dev; curl -o /etc/yum/repos.d/pl-build-tools-cisco-wrlinux-5.repo http://pl-build-tools.delivery.puppetlabs.net/yum/cisco-wrlinux/5/pl-build-tools-cisco-wrlinux-5.repo"
+  plat.provision_with "rpm -e libgmp-dev libmpc-dev libmpfr-dev"
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/cisco-wrlinux/5/pl-build-tools-cisco-wrlinux-5.repo"
   plat.install_build_dependencies_with "yum install -y"
   plat.vcloud_name "cisco-wrlinux-5-x86_64"
 end
