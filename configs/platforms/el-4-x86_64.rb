61c9be2345ec6ee622df9dd4b74c653e622c291f
Matthaus Owens <matthaus@puppetlabs.com>
2015-11-03 13:10:58 -0800

    (RE-5947) Use pl-tar on el4
    Now that tar is built and available for el4, this commit adds it to the
    packages to be installed and sets tar for the platform to be
    /opt/pl-build-tools/bin/tar. This allows things like binutils and gcc to
    correctly build again.

--- configs/platforms/el-4-x86_64.rb
+++ configs/platforms/el-4-x86_64.rb
@@ -2,9 +2,10 @@ platform "el-4-x86_64" do |plat|
   plat.servicedir "/etc/rc.d/init.d"
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
+  plat.tar "/opt/pl-build-tools/bin/tar"
 
   plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/el/4/x86_64/pl-build-tools-el-4.repo"
-  plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils ; yum update -y pkgconfig"
+  plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils pl-tar; yum update -y pkgconfig"
   # Installing libstdc++-devel needs to be a seperate transaction for it to
   # pull in 64-bit headers, why? I am not sure. I assume it's because in 2004
   # multilib was even more terrible than it is today.
