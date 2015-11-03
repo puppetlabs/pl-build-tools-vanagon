a0183758fe124052bc4ffb6ec986118a7070d0af
Matthaus Owens <matthaus@puppetlabs.com>
2015-11-03 12:38:19 -0800

    (RE-5947) Add tar to pl-build-tools-vanagon
    On certain older platforms, the tar is unusable for our builds. This
    commit adds a tar component and project to allow us to build a more
    modern tar for those systems.

---
+++ configs/projects/pl-tar.rb
@@ -0,0 +1,20 @@
+project "pl-tar" do |proj|
+  # Project level settings our components will care about
+  instance_eval File.read('configs/projects/pl-build-tools.rb')
+
+  proj.description "Puppet Labs Gnu Tar"
+  proj.version "1.28"
+  proj.license "GPLv3"
+  proj.vendor "Puppet Labs <info@puppetlabs.com>"
+  proj.homepage "https://www.puppetlabs.com"
+
+  # Platform specific
+  proj.setting(:cflags, "-I#{proj.includedir}")
+  proj.setting(:ldflags, "-L#{proj.libdir} -Wl,-rpath=#{proj.libdir}")
+
+  proj.component "tar"
+  proj.target_repo ""
+
+  proj.register_rewrite_rule 'http', 'http://buildsources.delivery.puppetlabs.net'
+
+end
