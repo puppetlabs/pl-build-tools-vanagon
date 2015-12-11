7694194739b1ea17e16fd9c7459856f523ffcf82
Melissa Stone <melissa@puppetlabs.com>
2015-12-11 00:21:47 +0000

    (maint) Include the --host flag if we've set it
    When we run the configure command on a system we're either cross
    compiling for, or on a system that's unique, we need to pass in the
    --host flag. This commit adds that to the iconv build. Currently, the
    only platform that sets --host is windows. Since we're building mingw
    packages in a cygwin system, we need to ensure we're setting host.

--- configs/components/iconv.rb
+++ configs/components/iconv.rb
@@ -26,7 +26,7 @@ component "iconv" do |pkg, settings, platform|
   end
 
   pkg.configure do
-    " ./configure --prefix=#{installation_prefix}"
+    " ./configure --prefix=#{installation_prefix} #{settings[:host]}"
   end
 
   pkg.build do
