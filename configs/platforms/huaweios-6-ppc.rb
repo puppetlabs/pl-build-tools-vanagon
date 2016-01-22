31ab00b2f015e79e40a3f65b2f4e7587e66637b2
Scott Garman <sgarman@zenlinux.com>
2016-01-21 20:53:19 -0800

    (RE-6257) Initial huaweios platform support
    This adds huaweios-6-ppc as a platform and includes necessary
    configuration to cross-compile pl-binutils for powerpc 32-bit.

---
+++ configs/platforms/huaweios-6-ppc.rb
@@ -0,0 +1,11 @@
+platform "huaweios-6-ppc" do |plat|
+  plat.servicedir "/lib/systemd/system"
+  plat.defaultdir "/etc/default"
+  plat.servicetype "systemd"
+  plat.codename "jessie"
+
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-jessie.deb"
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot"
+  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
+  plat.vmpooler_template "debian-8-x86_64"
+end
