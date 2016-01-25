74a5450244793ae355d843cf798cfd624ddb7847
Scott Garman <sgarman@zenlinux.com>
2016-01-24 21:03:43 -0800

    (maint) configure binutils with sysroot for huaweios
    --with-sysroot is an undocumented configure option in binutils
    but necessary to avoid "ld: this linker was not configured to
    use sysroots" when doing subsequent builds (e.g, pl-gcc)

--- configs/components/binutils.rb
+++ configs/components/binutils.rb
@@ -32,10 +32,17 @@ component "binutils" do |pkg, settings, platform|
   end
 
   pkg.configure do
+    if platform.is_huaweios?
+      # --with-sysroot is an undocumented configure option in binutils
+      # but necessary to avoid "ld: this linker was not configured to
+      # use sysroots" when doing subsequent builds (e.g, pl-gcc)
+      additional_flags = "--with-sysroot=#{File.join(settings[:prefix], 'sysroot')}"
+    end
     "./configure \
       --prefix=#{settings[:basedir]} \
       #{target} \
       --disable-nls \
+      #{additional_flags} \
       -v"
   end
 
