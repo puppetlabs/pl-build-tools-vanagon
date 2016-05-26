670aca64f0e1b1bb6d125a1ef18d1ff3be0622ea
Scott Garman <scott.garman@puppetlabs.com>
2016-05-26 10:34:27 -0700

    (maint) Use .repo files for el-6-s390x build-tools
    We can't use pl-build-tools-release packages for this platform because
    cross-compiled platforms need to have repo files for both their host
    and target architectures, and the release packages only ship a repo
    file that uses the OS's $basearch.

--- configs/platforms/el-6-s390x.rb
+++ configs/platforms/el-6-s390x.rb
@@ -3,8 +3,8 @@ platform "el-6-s390x" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/s390x/pl-build-tools-release-6-1.noarch.rpm"
-  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/x86_64/pl-build-tools-release-6-1.noarch.rpm"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/s390x/pl-build-tools-s390x.repo"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/x86_64/pl-build-tools-x86_64.repo"
   plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.cross_compiled true
