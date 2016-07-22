42ae1c35dcb2c262106fa325a4d3e8c7be002b23
Michael Stahnke <stahnma@puppetlabs.com>
2016-07-22 18:19:32 +0000

    (maint) Swap building of platforms to staging
    On this branch, we should have the platform definitions hitting staging
    in instead of hitting production. This means that we'll be able to test
    our newer package builds from staging.

--- configs/platforms/el-4-i386.rb
+++ configs/platforms/el-4-i386.rb
@@ -2,9 +2,9 @@ platform "el-4-i386" do |plat|
   plat.servicedir "/etc/rc.d/init.d"
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
-  plat.tar "/opt/pl-build-tools/bin/tar"
+  plat.tar "/opt/pl-build-tools-staging/bin/tar"
 
-  plat.add_build_repository  "http://pl-build-tools.delivery.puppetlabs.net/yum/el/4/i386/pl-build-tools-el-4.repo"
+  plat.add_build_repository  "http://pl-build-tools-staging.delivery.puppetlabs.net/yum/el/4/i386/pl-build-tools-staging-el-4.repo"
   plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils; yum update -y pkgconfig"
 
   plat.install_build_dependencies_with "yum install -y"
