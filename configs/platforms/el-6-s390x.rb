495f52e3b66224b34d4bb13aeb4fc031701e44d4
Michael Stahnke <stahnma@puppetlabs.com>
2016-07-22 18:19:32 +0000

    (maint) Swap building of platforms to staging
    On this branch, we should have the platform definitions hitting staging
    in instead of hitting production. This means that we'll be able to test
    our newer package builds from staging.

--- configs/platforms/el-6-s390x.rb
+++ configs/platforms/el-6-s390x.rb
@@ -3,8 +3,8 @@ platform "el-6-s390x" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/s390x/pl-build-tools-s390x.repo"
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/x86_64/pl-build-tools-x86_64.repo"
+  plat.add_build_repository "http://pl-build-tools-staging.delivery.puppetlabs.net/yum/el/6/s390x/pl-build-tools-staging-s390x.repo"
+  plat.add_build_repository "http://pl-build-tools-staging.delivery.puppetlabs.net/yum/el/6/x86_64/pl-build-tools-staging-x86_64.repo"
   plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.cross_compiled true
