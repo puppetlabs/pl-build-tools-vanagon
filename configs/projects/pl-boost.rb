856048e68ece5c9721d8b89e47f867761fd3b306
Michael Stahnke <stahnma@puppetlabs.com>
2016-08-01 18:41:26 +0000

    Update boost to 1.61

--- configs/projects/pl-boost.rb
+++ configs/projects/pl-boost.rb
@@ -10,6 +10,8 @@ project "pl-boost" do |proj|
     proj.version "1.58.0"
     proj.release "6"
   end
+
+  proj.description "Puppet Inc boost"
   proj.license "Boost and MIT and Python"
   proj.vendor "Puppet Labs <info@puppetlabs.com>"
   proj.homepage "https://www.puppetlabs.com"
