58c581042900dc6f5eee0d052ec8bb48e3e4cbbf
Michael Stahnke <stahnma@puppetlabs.com>
2016-08-02 00:55:50 +0000

    Update cmake everywhere

--- configs/projects/pl-cmake.rb
+++ configs/projects/pl-cmake.rb
@@ -3,7 +3,7 @@ project "pl-cmake" do |proj|
   instance_eval File.read('configs/projects/pl-build-tools.rb')
 
   proj.description "Puppet Labs cmake"
-  if platform.name =~ /fedora-f24/
+  if platform.name =~ /fedora-f24|debian-8-armel/
     proj.version "3.5.2"
     proj.release "0"
   else
