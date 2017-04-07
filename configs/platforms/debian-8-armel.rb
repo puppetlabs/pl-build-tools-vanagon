86c152854ec37eb80d6c31b7d0b70ca5c9ebe5ae
Michael Stahnke <stahnma@puppetlabs.com>
2017-04-07 04:12:58 +0000

    Do not use staging for debian-8-armel

--- configs/platforms/debian-8-armel.rb
+++ configs/platforms/debian-8-armel.rb
@@ -4,7 +4,7 @@ platform "debian-8-armel" do |plat|
   plat.servicetype "systemd"
   plat.codename "jessie"
 
-  plat.add_build_repository "http://pl-build-tools-staging.delivery.puppetlabs.net/debian/pl-build-tools-staging-release-jessie.deb"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-jessie.deb"
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update ; apt-get install -y --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot binutils-arm-linux-gnueabi"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
   plat.vmpooler_template "debian-8-x86_64"
