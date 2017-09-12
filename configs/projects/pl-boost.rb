3796ecd8daca5f453b7d68b487e1f96426beee5c
Scott Garman <scott.garman@puppet.com>
2017-09-12 23:31:51 +0000

    (maint) Use arch tag when building solaris 11 pl-boost pkgs
    This is needed for both i386 and sparc solaris 11 packages, and
    resolves errors complaining about the resulting pl-boost package
    confliciting with pl-gcc and pl-binutils.

--- configs/projects/pl-boost.rb
+++ configs/projects/pl-boost.rb
@@ -14,7 +14,7 @@ project "pl-boost" do |proj|
   proj.vendor "Puppet Labs <info@puppetlabs.com>"
   proj.homepage "https://www.puppetlabs.com"
 
-  if platform.is_cross_compiled?
+  if platform.is_cross_compiled? || platform.name =~ /solaris-11/
     proj.name "pl-boost-#{platform.architecture}"
     proj.noarch
   else
