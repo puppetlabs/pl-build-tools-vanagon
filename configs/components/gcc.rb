0e29b6a287aa4eb4311dbba1bc3b59486d346f92
Josh Cooper <joshcooper@users.noreply.github.com>
2022-10-04 10:37:05 -0700

    (PA-4578) Remove Debian 9

--- configs/components/gcc.rb
+++ configs/components/gcc.rb
@@ -1,6 +1,6 @@
 component "gcc" do |pkg, settings, platform|
   # Source-Related Metadata
-  if platform.name =~ /debian-9|el-7-aarch64|el-7-ppc64|sles-12-ppc64le|ubuntu-16\.04-ppc64el|ubuntu-18\.(04|10)/
+  if platform.name =~ /el-7-aarch64|el-7-ppc64|sles-12-ppc64le|ubuntu-16\.04-ppc64el|ubuntu-18\.(04|10)/
     pkg.version "6.1.0"
     pkg.md5sum "8d04cbdfddcfad775fdbc5e112af2690"
   elsif platform.is_aix? || platform.architecture =~ /arm/
