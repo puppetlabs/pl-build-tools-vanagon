83d6db3b3059858f246a2fc151aaa4deb743f263
isaac-hammes <isaac.hammes@puppet.com>
2022-10-05 14:07:36 -0700

    (maint) Use tar.xz for gmp versions greater than 5.0.2 and tar.gz for versions less than or equal to 5.0.2.

--- configs/components/gmp.rb
+++ configs/components/gmp.rb
@@ -1,6 +1,12 @@
 # https://gmplib.org
 component 'gmp' do |pkg, settings, _platform|
-  pkg.version settings[:gmp_version]
+  gmp_version = settings[:gmp_version]
+  pkg.version gmp_version 
   pkg.md5sum settings[:gmp_md5sum]
-  pkg.url "http://ftp.gnu.org/gnu/gmp/gmp-#{pkg.get_version}.tar.xz"
+  final_tar_gz_version = Gem::Version.new('5.0.2')
+  if Gem::Version.new(gmp_version) > final_tar_gz_version
+    pkg.url "http://ftp.gnu.org/gnu/gmp/gmp-#{gmp_version}.tar.xz"
+  else
+    pkg.url "http://ftp.gnu.org/gnu/gmp/gmp-#{gmp_version}.tar.gz"
+  end
 end
