9d18fb377cc30b55d138df3258a720da12515f9e
Eric Griswold <eric.griswold@puppet.com>
2023-03-22 14:30:22 -0700

    (maint) update pl-cmake for Cmake 3.26.0

--- configs/projects/pl-cmake.rb
+++ configs/projects/pl-cmake.rb
@@ -3,18 +3,19 @@ project "pl-cmake" do |proj|
   instance_eval File.read('configs/projects/pl-build-tools.rb')
 
   proj.description "Puppet Labs cmake"
-  proj.version "3.2.3"
-  proj.release "20"
+  proj.setting(:cmake_version, '3.26.0')
+  proj.version settings[:cmake_version]
+  proj.release '1'
 
-  proj.license "BSD"
-  proj.vendor "Puppet Labs <info@puppetlabs.com>"
-  proj.homepage "https://www.puppetlabs.com"
+  proj.license 'BSD 3-clause'
+  proj.vendor 'Puppet By Perforce <release@puppet.com>'
+  proj.homepage 'https://www.puppet.com'
 
   # Platform specific
   proj.setting(:cflags, "-I#{proj.includedir}")
   proj.setting(:ldflags, "-L#{proj.libdir} -Wl,-rpath=#{proj.libdir}")
 
-  proj.component "toolchain"
-  proj.component "cmake"
+  proj.component 'toolchain'
+  proj.component 'cmake'
   proj.target_repo ""
 end
